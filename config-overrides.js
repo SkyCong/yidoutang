fa9bfe25d6a234a991f5851d6a994f7ad4f99510
SkyCong <ssskycong@gmail.com>
2019-04-02 21:19:29 +0800

    V1.0.1

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+// const { injectBabelPlugin } = require('react-app-rewired');
+const path = require('path')
+module.exports = function override(config, env) {
+  // config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
+  config.module.rules[2].oneOf[1].options.babelrc = true;
+  
+  config.resolve.alias = {
+    'assets': path.resolve(__dirname, './src/assets'),
+    'pages': path.resolve(__dirname, './src/pages'),
+    'components': path.resolve(__dirname, './src/components'),
+    'utils': path.resolve(__dirname, './src/utils'),
+    '@': path.resolve(__dirname, './src')
+  }
+
+  return config;
+};
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        123 
+      </div>
+    );
+  }
+}
+
+export default App;
